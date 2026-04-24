30527f53ea81bdbbe7b93b7adab2b2d54872ec30
vycanismajoris1001-gif <vycanismajoris1001@gmail.com>
2026-04-24 09:23:40 +0900

    Update 2-1.c

--- code/2-1.c
+++ code/2-1.c
@@ -2,12 +2,12 @@
 
 int main() {
   char arr[] = "hello";
-  char* p = "hello";
+  char p = "hello";
 
   arr[0] = 'H';
   printf("%s\n", arr);
 
-  *p[0]= 'H';
+  p[0]= 'H';
   printf("%s\n", p);
 
   return 0;
