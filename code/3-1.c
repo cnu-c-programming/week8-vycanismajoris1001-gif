3407fb5046661e15e37ab5bd7cb08830bf79b007
vycanismajoris1001-gif <vycanismajoris1001@gmail.com>
2026-04-24 21:42:46 +0900

    Update 3-1.c

--- code/3-1.c
+++ code/3-1.c
@@ -4,7 +4,23 @@ int main() {
 
   char* names[] = {"apple", "mango", "ant", "banana", "cat", "anaconda"};
   int n = sizeof(names)/sizeof(names[0]);
+ for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - 1 - i; j++) {
+            
+            char* s1 = names[j];
+            char* s2 = names[j + 1];
 
+            int k = 0;
+            while (s1[k] != '\0' && s1[k] == s2[k]) {
+                k++;
+            }
+            if ((unsigned char)s1[k] > (unsigned char)s2[k]) {
+                char* temp = names[j];
+                names[j] = names[j + 1];
+                names[j + 1] = temp;
+            }
+        }
+    }
   for (int i = 0; i < n; i++) {
      printf("%s\n", names[i]);
   }
